6bb27be9ba3ae2df474109ae0fc8d9722c7b723a
Brage Aasen <brageaasen@Brages-MacBook-Air.local>
2023-03-05 14:00:28 +0100

    Fikset bug med tiles

--- src/main/resources/assets/tiles.tsx
+++ src/main/resources/assets/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="32" tileheight="32" tilecount="12" columns="3">
- <image source="Tilemap.png" width="96" height="128"/>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="Tilemap.png" width="160" height="128"/>
 </tileset>
