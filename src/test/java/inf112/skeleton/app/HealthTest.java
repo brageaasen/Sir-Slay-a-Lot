216e23bc7e541dc74c477495b8444dc43c3363fa
kavmal7 <kavyamal74@gmail.com>
2023-03-09 11:53:38 +0100

    Added JUnit tests for Health

--- src/test/java/inf112/skeleton/app/HealthTest.java
+++ src/test/java/inf112/skeleton/app/HealthTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Testing Health.java
+ */
 public class HealthTest {
     
     Health healthModel;
