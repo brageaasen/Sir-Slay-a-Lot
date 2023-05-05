b6ff02c648168860e2dc1faf78a993a04263ec20
Brage Aasen <brageaasen@Brages-Air.home>
2023-05-05 21:37:47 +0200

    Removed invalid tests.

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -85,19 +85,6 @@ public class PlayerTest {
     }
 
     /**
-     * Tests whether the Player is dead when health is 0
-     */
-    @Test 
-    void testUpdateSprite(){
-        player.setAttack(true);
-        // animationHandler.setState(PlayerState.Attack);
-        assertTrue(player.getAnimationHandler().getState() == PlayerState.Idle);
-        player.updateSprite();
-        assertTrue(player.getAnimationHandler().getState() == PlayerState.Attack);
-    }
-
-
-    /**
      * Test getter for kill count
      */
     @Test 
