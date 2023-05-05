9620e39a41947e798930fe05a4eb7dbb6162aded
Tarjei Landøy <tarjei.landoy@student.uib.no>
2023-05-05 16:48:04 +0200

    Update Gun tests

--- src/test/java/inf112/skeleton/app/GunTest.java
+++ src/test/java/inf112/skeleton/app/GunTest.java
@@ -1,13 +1,12 @@
 package inf112.skeleton.app;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import org.mockito.Mockito;
 
 import inf112.skeleton.app.Weapons.Gun;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -15,6 +14,56 @@ import org.junit.jupiter.api.Test;
 
 
 public class GunTest {
+
+    /**
+     * Simple testing of the gun that requires an actual gun (not mocked)
+     */
+    @Test
+    void testGunFire() {
+        Gun gun = new Gun(1f, 1, 10, 2f, new Sprite(), new Sprite());
+
+        // Test that the gun doesn't just throw after initialization.
+        assertDoesNotThrow(() -> gun.update(1f));
+
+        // Simple firing test
+        int bullets = gun.bulletsInChamber();
+        assertEquals(0, gun.getBullets().size());
+        assertDoesNotThrow(() -> gun.fire(new Vector2(0,0), new Vector2(1,0)));
+        assertEquals(bullets - 1, gun.bulletsInChamber());
+        assertEquals(1, gun.getBullets().size());   // fired once
+
+        var bullet0pos = gun.getBullets().get(0).getPosition();
+        assertDoesNotThrow(() -> gun.update(1f));
+        assertEquals(bullet0pos.add(new Vector2(1,0)), gun.getBullets().get(0).getPosition());
+        assertDoesNotThrow(() -> gun.update(1f));   // fire rate is 2f, need to wait out timer (update twice) to fire.
+        assertEquals(bullet0pos.add(new Vector2(1,0)), gun.getBullets().get(0).getPosition());
+
+        assertDoesNotThrow(() -> gun.fire(new Vector2(0,0), new Vector2(1,0)));
+        assertEquals(2, gun.getBullets().size());   // fired twice
+
+        assertEquals(8, gun.getBullets().get(0).getRange()); // update called twice after first fired bullet.
+        assertEquals(10, gun.getBullets().get(1).getRange()); // update NOT called since second fired bullet.
+        for (int i = 1; i < 8; i++) {
+            assertDoesNotThrow(() -> gun.update(1f));
+            assertEquals( 8 - i, gun.getBullets().get(0).getRange());
+            assertEquals(bullet0pos.add(new Vector2(1,0)), gun.getBullets().get(0).getPosition());
+            assertEquals(10 - i, gun.getBullets().get(1).getRange());
+        }
+        // Now, the first will be removed the next update.
+        assertDoesNotThrow(() -> gun.update(1f));
+        assertEquals(1, gun.getBullets().size());
+        // Two more and the second bullet is also removed.
+        assertDoesNotThrow(() -> gun.update(1f));
+        assertDoesNotThrow(() -> gun.update(1f));
+        assertTrue(gun.getBullets().isEmpty());
+
+        // Test that the other methods work.
+        assertDoesNotThrow(() -> gun.setPosition(new Vector2(1,2)));
+        assertEquals(1, gun.getSprite().getX());
+        assertEquals(2, gun.getSprite().getY());
+
+        assertDoesNotThrow(gun::getSprite);
+    }
 
     /**
      * Tests setter and getter for holding the Gun
@@ -43,7 +92,7 @@ public class GunTest {
     }
 
     /**
-     * Tests extra ammunition powerup for Gun
+     * Tests extra ammunition PowerUp for Gun
      */
     @Test 
     void testPowerUp(){
